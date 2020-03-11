5370f53195a4125a963cad1189d6238d9bf721d1
cvtran <cvtran31@gmail.com>
2020-03-10 19:23:20 -0700

    TestNG
    TestNG basics - Using xml to run multiple tests

---
+++ TestNG/src/test/Example1.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+	@Test
+	public void Demo1() {
+		System.out.println("test1"); //automation
+	}
+	
+	@Test
+	public void Demo2() {
+		System.out.println("test2"); //automation
+	}
+}
---
+++ TestNG/src/test/Example2.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Example2 {
+
+	@Test
+	public void Demo3() {
+		System.out.println("test3"); //automation
+	}
+}
