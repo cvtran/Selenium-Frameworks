a43471b733ed70a3588dd0f9385ea40836151291
cvtran <cvtran31@gmail.com>
2020-03-20 15:30:11 -0700

    Helper Attributes

--- TestNG/src/test/Example3.java
+++ TestNG/src/test/Example3.java
@@ -1,5 +1,26 @@
 package test;
 
-public class Example3 {
+import org.testng.annotations.Test;
 
+public class Example3 {
+	
+	//if test takes too long, set a timeOut so it wont fail
+	@Test(timeOut = 400)
+	public void WebSigninCarLoan() {
+		//selenium
+		System.out.println("web signin");
+	}
+	
+	@Test(enabled = false)
+	public void WebSignoutCarLoan() {
+		//selenium
+		System.out.println("web sign out");
+	}
+	
+	//dependsOnMethods makes sure that the test executes before this test
+	@Test(dependsOnMethods = {"WebSigninCarLoan"})
+	public void APICarLoan() {
+		//rest api automation
+		System.out.println("api login car");
+	}
 }
