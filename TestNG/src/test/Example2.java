8bee2c69bc09e5e74bd01746e5e2e24ea0d93b6f
cvtran <cvtran31@gmail.com>
2020-03-20 15:40:49 -0700

    Parameterising
    TestNG

--- TestNG/src/test/Example2.java
+++ TestNG/src/test/Example2.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Example2 {
@@ -11,13 +12,13 @@ public class Example2 {
 	//@BeforeSuite method executes before all methods in the suite have executed
 	@BeforeSuite
 	public void BeforeSuite() {
-		System.out.println("before suite");
+		System.out.println("----- before suite -----");
 	}
 	
 	//@AfterSuite method executes after all methods in the suite have executed
 	@AfterSuite
 	public void AfterSuite() {
-		System.out.println("after suite");
+		System.out.println("----- after suite----- ");
 	}
 	
 	//@BeforeMethod method executes before every @test method in this class (executes once for each test method)
@@ -29,13 +30,15 @@ public class Example2 {
 	//@AfterMethod method executes after every @test method in this class (executes once for each test method)
 	@AfterMethod
 	public void AfterMethod() {
-		System.out.println("after method");
+		System.out.println("after method \n");
 	}
 	
+	@Parameters({"URL"})
 	@Test
-	public void WebLoginCarLoan() {
+	public void WebLoginCarLoan(String urlName) {
 		//selenium
 		System.out.println("web login car");
+		System.out.println(urlName);
 	}
 	
 	@Test(groups = {"Mobile"})
