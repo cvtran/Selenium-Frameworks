9758d97f727d7fe065ed81d2cfcb6c6cf5ed6ea4
cvtran <cvtran31@gmail.com>
2020-03-20 15:12:02 -0700

    Annotations
    TestNG Annotations

--- TestNG/src/test/Example1.java
+++ TestNG/src/test/Example1.java
@@ -1,16 +1,32 @@
 package test;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Example1 {
 
+	//@BeforeClass runs before any @test methods in the class
+	@BeforeClass
+	public void BeforeClass() {
+		System.out.println("before class");
+	}
+	
+	//@AfterClass runs after any @test methods in the class
+	@AfterClass
+	public void AfterClass() {
+		System.out.println("after class");
+	}
+	
 	@Test
 	public void WebLoginHomeLoan() {
 		//selenium
 		System.out.println("web login home");
 	}
 	
-	@Test
+	@Test(groups = {"Mobile"})
 	public void MobileLoginHomeLoan() {
 		//appium
 		System.out.println("mobile login home");
@@ -20,5 +36,17 @@ public class Example1 {
 	public void LoginAPIHomeLoan() {
 		//rest api automation
 		System.out.println("api login home");
+	}
+	
+	//@BeforeTest method executes before all methods in the test have executed
+	@BeforeTest
+	public void BeforeTest() {
+		System.out.println("before test");
+	}
+	
+	//@AfterTest method executes after all methods in the test have executed
+	@AfterTest
+	public void AfterTest() {
+		System.out.println("after test");
 	}
 }
--- TestNG/src/test/Example2.java
+++ TestNG/src/test/Example2.java
@@ -1,16 +1,44 @@
 package test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Example2 {
 
+	//@BeforeSuite method executes before all methods in the suite have executed
+	@BeforeSuite
+	public void BeforeSuite() {
+		System.out.println("before suite");
+	}
+	
+	//@AfterSuite method executes after all methods in the suite have executed
+	@AfterSuite
+	public void AfterSuite() {
+		System.out.println("after suite");
+	}
+	
+	//@BeforeMethod method executes before every @test method in this class (executes once for each test method)
+	@BeforeMethod
+	public void BeforeMethod() {
+		System.out.println("before method");
+	}
+	
+	//@AfterMethod method executes after every @test method in this class (executes once for each test method)
+	@AfterMethod
+	public void AfterMethod() {
+		System.out.println("after method");
+	}
+	
 	@Test
 	public void WebLoginCarLoan() {
 		//selenium
 		System.out.println("web login car");
 	}
 	
-	@Test
+	@Test(groups = {"Mobile"})
 	public void MobileLoginCarLoan() {
 		//appium
 		System.out.println("mobile login car");
@@ -21,4 +49,5 @@ public class Example2 {
 		//rest api automation
 		System.out.println("api login car");
 	}
+
 }
