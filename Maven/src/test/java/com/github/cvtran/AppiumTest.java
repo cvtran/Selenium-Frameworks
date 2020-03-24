8ae42ae3cae93fa771fe7f38823d6fd4574c9e5a
cvtran <cvtran31@gmail.com>
2020-03-24 16:19:23 -0700

    Maven
    Maven Tutorial

---
+++ Maven/src/test/java/com/github/cvtran/AppiumTest.java
@@ -0,0 +1,18 @@
+package com.github.cvtran;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void AppAutomation() {
+		//Appium code
+		System.out.println("Open App");
+	}
+	
+	@Test
+	public void Login() {
+		//Appium code
+		System.out.println("Login App");
+	}
+}
---
+++ Maven/src/test/java/com/github/cvtran/SeleniumTest.java
@@ -0,0 +1,18 @@
+package com.github.cvtran;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation() {
+		//Selenium code
+		System.out.println("Open Broswer");
+	}
+	
+	@Test
+	public void Login() {
+		//Selenium code
+		System.out.println("Login Website");
+	}
+}
